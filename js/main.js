6ca46f968b41bac729543340603382c36a54a064
Jorgerm04 <jorgeroch2004@gmail.com>
2023-04-06 01:07:34 +0200

    Version 0.7
    Implementacion de animacion cuando enviamos formulario, estructura basica del html de productos.html y cuando la anchura de la pantalla es menor 555px el nav se convierte en un icono que se despliega al darle click y al darle otro se cierra

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+function enviar(){
+    swal("Bien hecho!", "Se ha enviado correctamente!", "success");
+
+}
+
+
+  
