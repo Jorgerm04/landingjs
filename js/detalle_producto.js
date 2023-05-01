e699ff2a26925e9cd2d0c475f865e48d09cbabeb
unaig2878 <unaig2878@gmail.com>
2023-05-01 16:36:02 +0200

    Version 0.15
    Funcionalidad del precio optimizada

--- js/detalle_producto.js
+++ js/detalle_producto.js
@@ -1,181 +1,35 @@
-
 function actualizarPrecio() {
       // Obtener los valores seleccionados de los desplegables
       var longitud = document.getElementById("desplegable-longitud").value;
       var diametro = document.getElementById("desplegable-diametro").value;
       var material = document.getElementById("desplegable-material").value;
-
-           
-      // Actualizar el precio dependiendo del valor que este seleccionado
-      switch (true) {
-            //detecta longitud diametro y material y en consecuencia le cambia el html al que pongamos en el .innerhtml
-            case longitud === "200" && diametro === "2,7" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "10.00€";
-                  break;
-            case longitud === "200" && diametro === "2,7" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "11.00€";
-                  break;
-            case longitud === "200" && diametro === "2,7" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "12.00€";
-                  break;
-            case longitud === "200" && diametro === "2,8" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "13.00€";
-                  break;
-            case longitud === "200" && diametro === "2,8" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "14.00€";
-                  break;
-            case longitud === "200" && diametro === "2,8" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "15.00€";
-                  break;
-            case longitud === "200" && diametro === "2,9" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "16.00€";
-                  break;
-            case longitud === "200" && diametro === "2,9" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "17.00€";
-                  break;
-            case longitud === "200" && diametro === "2,9" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "18.00€";
-                  break;
-            case longitud === "200" && diametro === "3,0" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "19.00€";
-                  break;
-            case longitud === "200" && diametro === "3,0" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "20.00€";
-                  break;
-            case longitud === "200" && diametro === "3,0" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "21.00€";
-                  break;
-            case longitud === "200" && diametro === "3,1" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "22.00€";
-                  break;
-            case longitud === "200" && diametro === "3,1" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "23.00€";
-                  break;
-            case longitud === "200" && diametro === "3,1" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "24.00€";
-                  break;
-            case longitud === "200" && diametro === "3,2" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "25.00€";
-                  break;
-            case longitud === "200" && diametro === "3,2" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "26.00€";
-                  break;
-            case longitud === "200" && diametro === "3,2" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "27.00€";
-                  break;
-            case longitud === "200" && diametro === "3,3" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "28.00€";
-                  break;
-            case longitud === "200" && diametro === "3,3" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "29.00€";
-                  break;
-            case longitud === "200" && diametro === "3,3" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "30.00€";
-                  break;
-            case longitud === "200" && diametro === "3,4" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "31.00€";
-                  break;
-            case longitud === "200" && diametro === "3,4" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "32.00€";
-                  break;
-            case longitud === "200" && diametro === "3,4" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "33.00€";
-                  break;
-            case longitud === "200" && diametro === "3,5" && material == "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "34.00€";
-                  break;
-            case longitud === "200" && diametro === "3,5" && material == "poliester":
-                  document.getElementById("precio-producto").innerHTML = "35.00€";
-                  break;
-            case longitud === "200" && diametro === "3,5" && material == "nylon":
-                  document.getElementById("precio-producto").innerHTML = "36.00€";
-                  break;
-
-            case longitud === "500" && diametro === "2,7" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "10.00€";
-                  break;
-            case longitud === "500" && diametro === "2,7" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "11.00€";
-                  break;
-            case longitud === "500" && diametro === "2,7" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "12.00€";
-                  break;
-            case longitud === "500" && diametro === "2,8" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "13.00€";
-                  break;
-            case longitud === "500" && diametro === "2,8" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "14.00€";
-                  break;
-            case longitud === "500" && diametro === "2,8" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "15.00€";
-                  break;
-            case longitud === "500" && diametro === "2,9" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "16.00€";
-                  break;
-            case longitud === "500" && diametro === "2,9" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "17.00€";
-                  break;
-            case longitud === "500" && diametro === "2,9" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "18.00€";
-                  break;
-            case longitud === "500" && diametro === "3,0" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "19.00€";
-                  break;
-            case longitud === "500" && diametro === "3,0" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "20.00€";
-                  break;
-            case longitud === "500" && diametro === "3,0" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "21.00€";
-                  break;
-            case longitud === "500" && diametro === "3,1" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "22.00€";
-                  break;
-            case longitud === "500" && diametro === "3,1" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "23.00€";
-                  break;
-            case longitud === "500" && diametro === "3,1" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "24.00€";
-                  break;
-            case longitud === "500" && diametro === "3,2" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "50.00€";
-                  break;
-            case longitud === "500" && diametro === "3,2" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "26.00€";
-                  break;
-            case longitud === "500" && diametro === "3,2" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "27.00€";
-                  break;
-            case longitud === "500" && diametro === "3,3" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "28.00€";
-                  break;
-            case longitud === "500" && diametro === "3,3" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "29.00€";
-                  break;
-            case longitud === "500" && diametro === "3,3" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "30.00€";
-                  break;
-            case longitud === "500" && diametro === "3,4" && material === "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "31.00€";
-                  break;
-            case longitud === "500" && diametro === "3,4" && material === "poliester":
-                  document.getElementById("precio-producto").innerHTML = "32.00€";
-                  break;
-            case longitud === "500" && diametro === "3,4" && material === "nylon":
-                  document.getElementById("precio-producto").innerHTML = "33.00€";
-                  break;
-            case longitud === "500" && diametro === "3,5" && material == "polipropileno":
-                  document.getElementById("precio-producto").innerHTML = "34.00€";
-                  break;
-            case longitud === "500" && diametro === "3,5" && material == "poliester":
-                  document.getElementById("precio-producto").innerHTML = "35.00€";
-                  break;
-            case longitud === "500" && diametro === "3,5" && material == "nylon":
-                  document.getElementById("precio-producto").innerHTML = "36.00€";
-                  break
-
-      }
-      
-}
+    
+      // Precios base para cada material
+      var preciosBase = {
+        polipropileno: 10,
+        poliester: 11,
+        nylon: 12
+      };
+    
+      // Verificar si el material es válido
+      if (preciosBase.hasOwnProperty(material)) {
+        // Calcular el precio base del material seleccionado
+        var precioBase = preciosBase[material];
+    
+        // Calcular el incremento por diámetro
+        var incrementoDiametro = (parseFloat(diametro) - 2.7) * 3;
+    
+        // Calcular el incremento por longitud
+        var incrementoLongitud = longitud === "500";
+    
+        // Calcular el precio final
+        var precioFinal = precioBase + incrementoDiametro + incrementoLongitud;
+    
+        // Actualizar el HTML con el precio calculado
+        document.getElementById("precio-producto").innerHTML = precioFinal.toFixed(2) + "€";
+      } 
+    }
+    
 
 
 //Funciones para que al poner el raton encima de uno de los divs se cambien a la vez el color de fondo y para que vuelvan a su color anterior
